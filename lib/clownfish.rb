8c38760440ffa76011cfc94e50a1ea70482e7a75
Paul Salaets <psalaets@gmail.com>
2013-02-20 17:38:35 -0500

    move fish into their own dir

--- lib/clownfish.rb
+++ lib/clownfish.rb
@@ -2,10 +2,8 @@ require "clownfish/version"
 require "clownfish/adapter"
 require "clownfish/anemone_ext"
 
-# helpers must be before clownfish impls
 require "clownfish/helpers/status_group"
 require "clownfish/helpers/url_statuses"
 
-# clownfish impls:
-require "clownfish/links_by_page"
-require "clownfish/response_times"
+require "clownfish/fish/links_by_page"
+require "clownfish/fish/response_times"
