e9f6838b9fec6470064eb6da9e54a6a1ce4b3fec
Paul Salaets <psalaets@gmail.com>
2013-02-27 09:59:19 -0500

    comment

--- lib/clownfish/fish/count.rb
+++ lib/clownfish/fish/count.rb
@@ -1,6 +1,7 @@
 module Clownfish
   # Clownfish that counts number of pages on a site.  Taken from Anemone.
   class Count
+    # Number of pages found.  Only meaningful after a crawl.
     attr_reader :count
 
     def after_crawl(page_store)
